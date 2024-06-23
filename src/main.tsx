eaeeb2a24d47e4173a60d9e9d6036d2572a278d5
Ysaac Andujar Binet <ysaac@codika.net>
2024-06-22 23:43:00 -0400

    setup axios

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { setupAxios } from 'config/axios.ts'
 
+setupAxios()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.Suspense>
     <App />
