42fd8c242b105be274da3d063541a3963041d3af
Ysaac Andujar Binet <ysaac@codika.net>
2024-06-24 21:08:16 -0400

    didclose

--- src/utils/modal.ts
+++ src/utils/modal.ts
@@ -1,9 +1,10 @@
 import Swal from 'sweetalert2'
 
-export const showModal= ({title, text, type}:{title:string, text:string, type: 'success' | 'warning' | 'error'}) =>{
+export const showModal= ({title, text, type, didClose}:{title:string, text:string, type: 'success' | 'warning' | 'error', didClose?: () => void}, ) =>{
     return Swal.fire({
         title,
         text,
-        icon: type
+        icon: type,
+        didClose,
       });
 }
